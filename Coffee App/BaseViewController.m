18c9c2115ffc8502ec33950405b4f83ba5ae6c5f
Sonny Back <Sonny@Sonnys-MacBook-Pro.local>
2014-03-17 22:05:08 -0400

    initial rendering of images

--- Coffee App/BaseViewController.m
+++ Coffee App/BaseViewController.m
@@ -20,7 +20,43 @@
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
-    NSLog(@"BaseViewController.viewDidLoad...");
+    NSLog(@"BaseViewController.viewDidLoad!");
+    
+    [self createImageViewer];
+}
+
+/**
+ * Method to create the UIImageView to contain the images to display
+ *
+ * @return void
+ */
+- (void)createImageViewer {
+    
+    NSLog(@"Entered createImageViewer");
+    // size attributes of the main view and scroll view
+    CGFloat imageViewHeight = self.imageView.frame.size.height; // height of the main view
+    CGFloat imageViewWidth = self.imageView.frame.size.width; // width of the the main view
+    CGFloat scrollViewHeight = self.scrollView.frame.size.height; // height of the scroll view
+    CGFloat scrollViewWidth = self.scrollView.frame.size.width; // width of the scroll view
+    
+    // location of the scroll view
+    CGFloat scrollViewLocationX = self.scrollView.frame.origin.x;
+    CGFloat scrollViewLocationY = self.scrollView.frame.origin.y;
+    
+    NSLog(@"imageView width: %f, imageView height: %f", imageViewWidth, imageViewHeight);
+    NSLog(@"scrollView width: %f, scrollView height: %f", scrollViewWidth, scrollViewHeight);
+    
+    // create the image view and set its location equal to the scroll view
+    UIImageView *imgView = [[UIImageView alloc] initWithFrame:CGRectMake(scrollViewLocationX, scrollViewLocationY, scrollViewWidth, scrollViewHeight)];
+    NSLog(@"imgView width: %f, imgView height: %f", imgView.bounds.size.width, imgView.bounds.size.height);
+    imgView.contentMode = UIViewContentModeScaleAspectFill;
+    imgView.clipsToBounds = YES;
+    //imgView.backgroundColor = [UIColor blackColor]; // just for testing
+    UIImage *testImage = [UIImage imageNamed:[NSString stringWithFormat:@"creamy_frozen_coffee"]]; // hard coded for testing only
+    imgView.image = testImage;
+    
+    [self.imageView addSubview:imgView]; // add the image view to the main view (imageView)
+    
 }
 
 - (void)didReceiveMemoryWarning
